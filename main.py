231fd251505fdd58b2922a8a056cf4dbb68fdbd3
luiz felipe <luizfpereira406@gmail.com>
2023-09-25 19:14:34 -0300

    Add flask restful library

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
-CORS(app)
+api = Api(app)
 
-@app.route("/hi/<name>")
-def helloWorld(name):
-    return {
-        "message": "hello world, " + name
-    }
+class HelloWorld(Resource):
+    def get(self, name):
+        return {
+            "message": "hello world, " + name
+        }
+
+api.add_resource(HelloWorld, "/hi/<string:name>")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
