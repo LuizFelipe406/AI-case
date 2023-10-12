f522ad03b10318fead63ffad9c2e2d541acdce01
luiz felipe <luizfpereira406@gmail.com>
2023-10-12 19:40:35 -0300

    Finish saving data from response in DB

--- controllers/start_game.py
+++ controllers/start_game.py
@@ -1,9 +1,13 @@
 import re
 import random
+import json
 from flask_restful import Resource
 from flask import request
 from open_ai.open_ai import OpenAI
 from rpg_basic_rules import rpg_roles, rpg_boss
+from sqlalchemy import delete
+from database.database import db
+from database.models import Character, Game
 
 class StartGame(Resource):
     def __init__(self):
@@ -27,7 +31,8 @@ class StartGame(Resource):
 
           you will be given a list of names and must create the rpg characters for each player and return each one of them in a Javascript object, following this rules:
             - name list order needs to be reordered randomly
-            - every player must have  a different role, of which must be choosen randomly 
+            - every player must have  a different role, of which must be choosen randomly
+            - players can have the same type, it shouldn't be unique, but must be choosen randomly
             - this part of the answer will start with the title: "Creating RPG Characters:"
             - you must generate a medieval last name for each character that matches his role, be creative here and increse the temperature of this part
             - you must choose his stats of attack, life, defense and lucky between the values determined in each class
@@ -44,23 +49,48 @@ class StartGame(Resource):
 """
         response = self.open_ai.generate_answer(context, player_list)
         answer = response.choices[0].message.content
-        return self.collect_data(answer, player_list)
+        try:
+          return self.collect_data(answer, player_list)
+        except Exception as e:
+            return { "error": "chat gpt did not respond in correct formart, try again please"}, 500
     
     def collect_data(self, answer: str, player_list: list):
         answer = answer.replace("\n", "")
-        game_id = re.search(r'\d{5}', answer, re.M)
-        
-        characters = re.findall(r'{[\s\S]*?}', answer, re.M)
+        game = Game(
+            game_id=re.search(r'\d{5}', answer, re.M).group(),
+            story=re.search(r'(?<=The\sQuest:)[\s\S]*', answer, re.M).group(),
+            status='on going'
+            )
 
-        for i in range(len(player_list) + 1):
-            character = characters[i]
-            #save in db
+        db.session.add(game)
+        db.session.commit()
         
-        story_context = re.search(r'(?<=The\sQuest:)[\s\S]*', answer, re.M)
+        try:
+          characters: list[str] = re.findall(r'{[\s\S]*?}', answer, re.M)
+          for i in range(len(player_list) + 1):
+              character_dict = json.loads(characters[i])
+              character_model = Character(
+                game_id=game.game_id,
+                role=character_dict["role"],
+                type=character_dict["type"],
+                name=character_dict["name"],
+                life=character_dict["life"],
+                defense=character_dict["defense"],
+                lucky=character_dict["lucky"],
+                attack=character_dict.get("attack", None),
+                heal=character_dict.get("heal", None),
+              )
+              db.session.add(character_model)
+        except Exception as e:
+            db.session.delete(game)
+            db.session.execute(
+                delete(Character).where(Character.game_id == game.game_id)
+            )
+            raise Exception
 
+        db.session.commit()
         return {
-            "answer": answer,
-            "game_id": game_id.group(),
+            "game_id": game.game_id,
             "characters": characters,
-            "story": story_context.group()
+            "story": game.story
         }
